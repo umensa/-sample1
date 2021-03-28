6ab2c4aabc1faa095200c0682dd897c65e279531
Mohamed Zouari <mensae@posteo.org>
2021-03-28 20:19:04 +0100

    Method Return Values

--- parameters.rb
+++ parameters.rb
@@ -1,14 +1,10 @@
-def add(first, second)
-	puts first, second
-	puts first + second
-end
-
 def substract(first, second)
-	puts first, second
-	puts first - second
+	first - second
 end
 
-add 100, 50
-substract 75, 25
-add 3, 4
-substract 10, 5
+number = 9
+puts number
+number = substract(number, 1)
+puts number
+number = substract(number, 1)
+puts number
--- widgets.rb
+++ widgets.rb
@@ -1,1 +1,9 @@
+
+def ask(question)
+	print question
+	gets
+end
+
 puts "Welcome to the widget store!"
+answer = ask("How many widgets are you ordering?")
+puts answer
