391b866cfede02e936c6a8b1f6d42215300b1a31
Mohamed Zouari <mensae@posteo.org>
2021-03-29 08:59:49 +0100

    String Concatenation

--- widgets.rb
+++ widgets.rb
@@ -1,9 +1,9 @@
 
 def ask(question)
-	print question
+	print question + " "
 	gets
 end
 
 puts "Welcome to the widget store!"
 answer = ask("How many widgets are you ordering?")
-puts answer
+puts "you entered " + answer + " widgets"
