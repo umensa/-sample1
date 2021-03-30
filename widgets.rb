654380e71001505b39df3de432a0f6a7158c59e0
Mohamed Zouari <mensae@posteo.org>
2021-03-30 07:49:38 +0100

    Comparison Operators

--- widgets.rb
+++ widgets.rb
@@ -1,9 +1,15 @@
 def ask(question)
 	print question + " "
-	gets.chomp
+	gets.chomp # Add ".chomp" here
 end
+
+# New method here
+def price(quantity)
+ 	quantity * 10
+ end
 
 puts "Welcome to the widget store!"
 answer = ask("How many widgets are you ordering?")
 number = answer.to_i
-puts "For #{answer} widgets, your total is: $#{number * 10}"
+total = price(number) # Call new method
+puts "For #{number} widgets, your total is: $#{total}"
