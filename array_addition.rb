3efd218519204132cc957336a47441f3643e1af1
Mohamed Zouari <mensae@posteo.org>
2021-03-30 21:36:29 +0100

    Adding Items to Arrays

---
+++ array_addition.rb
@@ -0,0 +1,8 @@
+grocery_list = ["milk", "eggs", "bread"]
+grocery_list << "carrots"
+grocery_list.push("potatos")
+grocery_list.unshift("celery")
+grocery_list += ["ice cream", "pie"]
+grocery_list << 1
+
+puts grocery_list.inspect
