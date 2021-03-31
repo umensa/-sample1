f2d265dc85dc9e207417b369f5d0b53493c02a2e
Mohamed Zouari <mensae@posteo.org>
2021-03-31 15:01:53 +0100

    Build a Grocery List Program: Part 2

--- shopping_list.rb
+++ shopping_list.rb
@@ -2,7 +2,7 @@ def create_list
 	print "What is the list name? "
 	name = gets.chomp
 
-	hash = {"name" => name, "item" => Array.new}
+	hash = {"name" => name, "items" => Array.new}
 	return hash
 end
 
@@ -10,9 +10,15 @@ def add_list_item
 	print "What is the item called? "
 	item_name = gets.chomp
 
-	hash = {"name" => name}
+	print "How much? "
+	quantity = gets.chomp.to_i
+
+	hash = {"name" => item_name, "quantity" => quantity}
 	return hash
 end
 
 list = create_list()
+puts list.inspect
+list['items'].push(add_list_item())
+
 puts list.inspect
