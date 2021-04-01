25838cd007cf20e240e2dcc431736a498dbec4ce
Mohamed Zouari <mensae@posteo.org>
2021-04-01 18:09:25 +0100

    Part 3: Printing The Contact List

--- ruby-loops/contact_list.rb
+++ ruby-loops/contact_list.rb
@@ -26,3 +26,15 @@ while answer != "n"
 	contact_list.push(add_contact())
 	answer = ask("Add another? (y/n)")
 end
+
+puts "---"
+
+contact_list.each do |contact|
+	puts "Name: #{contact["name"]}"
+	if contact["phone_numbers"].size > 0
+		contact["phone_numbers"].each do |phone_number|
+			puts "phone: #{phone_number}"
+		end
+	end	
+	puts "---\n"
+end
