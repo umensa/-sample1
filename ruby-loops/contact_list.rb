8ed81c9b53c28f919c08a6df8060b27e8d124371
Mohamed Zouari <mensae@posteo.org>
2021-04-01 17:56:19 +0100

    Part 2: Adding Contacts

--- ruby-loops/contact_list.rb
+++ ruby-loops/contact_list.rb
@@ -1,5 +1,3 @@
-contact_list = []
-
 def ask(question, kind="string")
 	print question + " "
 	answer = gets.chomp
@@ -7,5 +5,24 @@ def ask(question, kind="string")
 	return answer
 end
 
-answer = ask("What is your name?")
-puts answer
+def add_contact
+	contact = { "name" => "", "phone_numbers" => [] }
+	contact["name"] = ask("What is the person's name?")
+	answer = ""
+	while answer != "n"
+		answer = ask("Do you want to add a phone number? (y/n)")
+		if answer == "y"
+			phone = ask("Enter a phone number:")
+			contact["phone_numbers"].push(phone)
+		end
+	end
+	return contact
+end
+
+contact_list = []
+
+answer = ""
+while answer != "n"
+	contact_list.push(add_contact())
+	answer = ask("Add another? (y/n)")
+end
