9931a2de63ac71e477251d9a9a4ef09946fe0dcf
Mohamed Zouari <mensae@posteo.org>
2021-04-02 13:59:32 +0100

    Creating a Class

---
+++ ruby-objects-and-classes/name.rb
@@ -0,0 +1,23 @@
+class Name
+	def title
+		"Mr."
+	end
+
+	def first_name
+		"Mohamed"
+	end
+
+	def middle_name
+		""
+	end
+
+	def last_name
+		"Zouari"
+	end
+end
+
+name = Name.new
+puts name.title
+puts name.first_name
+puts name.middle_name
+puts name.last_name
