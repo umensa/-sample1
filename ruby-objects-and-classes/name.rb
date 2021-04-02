bf828e0e62aed8a054352f392c97ef4874350410
Mohamed Zouari <mensae@posteo.org>
2021-04-02 19:07:52 +0100

    Attribute Writers and Accessors

--- ruby-objects-and-classes/name.rb
+++ ruby-objects-and-classes/name.rb
@@ -1,5 +1,8 @@
 class Name
-	attr_reader :title, :first_name, :middle_name, :last_name
+	attr_reader :first_name, :middle_name, :last_name
+	attr_accessor :title
+#Ruby gives us shortcuts to write common pieces of code. 
+#The process of accessing and writing attributes in a class can be done using `attr_writer` and `attr_accessor` to ease repeating code.
 
 	def initialize(title, first_name, middle_name, last_name)
 		@title = title
@@ -14,3 +17,7 @@ puts name.title + " " +
 	name.first_name + " " +
 	name.middle_name + " " +
 	name.last_name
+
+puts "Title: #{name.title}"
+name.title = "Dr."
+puts "Title: #{name.title}"
