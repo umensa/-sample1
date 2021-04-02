feae9aeb07c2b70e7234a25c65a47dcc7f910111
Mohamed Zouari <mensae@posteo.org>
2021-04-02 18:43:03 +0100

    Attribute Readers

--- ruby-objects-and-classes/name.rb
+++ ruby-objects-and-classes/name.rb
@@ -1,25 +1,11 @@
 class Name
+	attr_reader :title, :first_name, :middle_name, :last_name
+
 	def initialize(title, first_name, middle_name, last_name)
 		@title = title
 		@first_name = first_name
 		@middle_name = middle_name
 		@last_name = last_name
-	end
-
-	def title
-		@title
-	end
-
-	def first_name
-		@first_name
-	end
-
-	def middle_name
-		@middle_name
-	end
-
-	def last_name
-		@last_name
 	end
 end
 
