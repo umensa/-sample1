edb4665537763b1964fc59b9826ca2daa3439039
Mohamed Zouari <mensae@posteo.org>
2021-04-03 08:53:22 +0100

    Part 2: Transactions, credits, and debits

--- ruby-objects-and-classes/bank_account.rb
+++ ruby-objects-and-classes/bank_account.rb
@@ -1,9 +1,26 @@
 class BankAccount
+	attr_reader :name
+
 	def initialize(name)
 		@name = name
 		@transactions = []
+		add_transaction("Begining Balance", 0)
+	end
+
+	def credit(description, amount)
+		add_transaction(description, amount)
+	end
+
+	def debit(description, amount)
+		add_transaction(description, -amount)
+	end
+
+	def add_transaction(description, amount)
+		@transactions.push(description: description, amount: amount)
 	end
 end
 
-bank_account = BankAccount.new("Jason")
+bank_account = BankAccount.new("Bambino")
+bank_account.credit("Paycheck", 100)
+bank_account.debit("Groceries", 40)
 puts bank_account.inspect
