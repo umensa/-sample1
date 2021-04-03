ea7d727a22d48e024be8a2a01e17e0a8926a4e39
Mohamed Zouari <mensae@posteo.org>
2021-04-03 08:25:41 +0100

    The to_s method

--- ruby-objects-and-classes/name.rb
+++ ruby-objects-and-classes/name.rb
@@ -1,6 +1,6 @@
 class Name 
 	attr_accessor :title, :first_name, :middle_name, :last_name
-	attr_reader :first_and_middle_name
+	
 	def initialize(title, first_name, middle_name, last_name)
 		@title = title
 		@first_name = first_name
@@ -9,15 +9,19 @@ class Name
 	end
 
 	def full_name
-		@first_and_middle_name = @first_name + " " + @middle_name
-		#@first_name + " " + @middle_name + " " + @last_name
-		@first_and_middle_name + " " + @last_name
+		@first_name + " " + @middle_name + " " + @last_name
 	end
 
 	def full_name_with_title
 		@title + " " + full_name()
 	end
+
+	def to_s
+		full_name_with_title
+	end
 end
 
 name = Name.new("Mr.", "Mohamed", "", "Zouari")
 puts name.full_name_with_title
+puts name
+puts name.inspect
