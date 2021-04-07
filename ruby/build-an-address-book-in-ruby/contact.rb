dd38b0a2c1782a9c5dbd93019b4a122ad505cce6
Mohamed Zouari <mensae@posteo.org>
2021-04-07 13:28:57 +0100

    Contact Class: Part 2

--- ruby/build-an-address-book-in-ruby/contact.rb
+++ ruby/build-an-address-book-in-ruby/contact.rb
@@ -16,6 +16,37 @@ class Contact
     @last_name
   end
 
+  def first_last
+    first_name + ' ' + last_name
+  end
+
+  def last_first
+    last_first = last_name
+    last_first += ', '
+    last_first += first_name
+    unless @middle_name.nil?
+      last_first += ' '
+      last_first += middle_name.slice(0, 1)
+      last_first += '.'
+    end
+    last_first
+  end
+
+  def to_s(format = 'full_name')
+    case format
+    when 'full_name'
+      full_name
+    when 'last_first'
+      last_first
+    when 'first'
+      first_name
+    when 'last'
+      last_name
+    else
+      first_last
+    end
+  end
+
   def full_name
     full_name = first_name
     unless @middle_name.nil?
@@ -31,10 +62,13 @@ end
 mohamed = Contact.new
 mohamed.first_name = 'Mohamed'
 mohamed.last_name = 'Zouari'
-puts mohamed.full_name
+puts mohamed.to_s
+puts mohamed.to_s('full_name')
+puts mohamed.to_s('last_first')
 
 nick = Contact.new
 nick.first_name = 'Nick'
 nick.middle_name = 'A'
 nick.last_name = 'Pettit'
-puts nick.full_name
+puts nick.to_s('full_name')
+puts nick.to_s('first_last')
